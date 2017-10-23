7475d00d290f6c7a628b7b79ed10291062de37b1
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-10-23 16:51:41 -0700

    tooltips working properly

--- code/js/train_times.js
+++ code/js/train_times.js
@@ -9,22 +9,25 @@ var margin = {top: 20, right: 30, bottom: 20, left: 100},
 var formatTime2 = d3.time.format("%I:%M");
 
 
-var x = d3.time.scale()
+var xscale = d3.time.scale()
     .domain([parseTime2("5:30"), parseTime2("24:00")])
     .range([0, trainwidth]);
 
-var y = d3.scale.linear()
+var yscale = d3.scale.linear()
     .domain([0, 5527])
     .range([0, trainheight]);
 
 var xAxis = d3.svg.axis()
-    .scale(x)
+    .scale(xscale)
     .ticks(8)
     .tickFormat(formatTime);
 
-
+var d3line2 = d3.svg.line()
+      .x(function(d){return d.x;})
+      .y(function(d){return d.y;})
+      .interpolate("linear");
 d3.csv("../data/hyperloop.csv", function(data) {
-  console.log(data);
+  //console.log(data);
   var svg2 = d3.select("#train").append("svg")
       .attr("width", trainwidth+margin.left+margin.right)
       .attr("height", trainheight+margin.top+margin.bottom)
@@ -40,7 +43,7 @@ d3.csv("../data/hyperloop.csv", function(data) {
   svg2.selectAll("text").data(cities).enter().append("text")
     .text(function (d) { return d; })
     .attr("x",0)
-    .attr("y", function (d, i) { return y(city_distances[i])})
+    .attr("y", function (d, i) { return yscale(city_distances[i])})
     .attr("fill", "black")
     .style("text-anchor", "end")
 
@@ -50,8 +53,8 @@ d3.csv("../data/hyperloop.csv", function(data) {
     .attr("stroke", "#ddd")
     .attr("stroke-dasharray", "1,1")
     .attr("stroke-width", 2)
-    .attr('y1', function (d, i) { return y(city_distances[i])})
-    .attr('y2', function (d, i) { return y(city_distances[i])})
+    .attr('y1', function (d, i) { return yscale(city_distances[i])})
+    .attr('y2', function (d, i) { return yscale(city_distances[i])})
 
   svg2.append("g")
     .attr("class", "x top axis")
@@ -64,29 +67,20 @@ d3.csv("../data/hyperloop.csv", function(data) {
 
   var stops = ["Seattle arrive" , "Seattle depart", "San Francisco arrive", "San Francisco depart", "Los Angeles arrive", "Los Angeles depart", "Austin arrive", "Austin depart", "Boulder arrive", "Boulder depart", "Chicago arrive", "Chicago depart", "New York arrive", "New York depart", "Boston arrive", "Boston depart"]
   var city_distances2 = [0, 0, 807, 807, 1190, 1190, 2567, 2567, 3503, 3503, 4521, 4521, 5311, 5311, 5527, 5527]
-
-  var i;
-  for (i = 0; i < data.length; i++) {
+  var schedules = []
+  /*for (var i = 0; i < data.length; i++) {
     schedule = []
     train_times = []
     train = data[i]["train_name"]
     console.log(data[i])
     string = train + " : " + JSON.stringify(data[i])
-    var htmlstring =  "<h5> Schedule </h5>"
-    for (var property in data[i]) {
-      if (data[i].hasOwnProperty(property) && property != "train_name") {
-        // do stuff
-        if (data[i][property] != "") {
-          htmlstring += property + " : " + data[i][property] + "<br>"
-        }
-      }
-    }
+   
     delete data[i].train_name
     var j = 0
     for (var property in data[i]) {
       time = data[i][property]
       if (time != "") {
-        schedule.push({x: x(parseTime2(time)), y: y(city_distances2[j])})
+        schedule.push({x: xscale(parseTime2(time)), y: yscale(city_distances2[j])})
         train_times.push({stop: stops[j], time: time})
       }
       j++;
@@ -94,17 +88,55 @@ d3.csv("../data/hyperloop.csv", function(data) {
     schedule.sort(function(a, b) {
       return a.x - b.x;
     });
-    var d3line2 = d3.svg.line()
-      .x(function(d){return d.x;})
-      .y(function(d){return d.y;})
-      .interpolate("linear");
+    schedules.push(schedule)
+      //.append("svg:title")
+      //.text(string);
+    }*/
+    var train = svg2.selectAll(".train")
+    .data(data)
+    .enter().append("g")
+      .attr("class", "train");
 
-    svg2.append("svg:path")
-      .attr("d", d3line2(schedule))
+    train.append("svg:path")
+      .attr("d", function(d) { 
+        //console.log(d)
+        schedule = []
+        var j = 0;
+        delete d.train_name
+        for (var property in d) {
+          //time = [property]
+
+          if (d[property] != "") {
+            /*console.log(d[property]);
+            console.log(city_distances2[j])
+            console.log(xscale(parseTime2(d[property])));
+            console.log(yscale(city_distances2[j]));*/
+            schedule.push({x: xscale(parseTime2(d[property])), y: yscale(city_distances2[j])})
+          }
+          j++;
+        }
+
+        //console.log(schedule);
+        schedule.sort(function(a, b) {
+      return a.x - b.x;
+    });
+         return d3line2(schedule);})
+     // .attr("d", function(d) { return d3line2(d);})
       .style("stroke-width", 2)
       .style("stroke", "steelblue")
       .style("fill", "none")
-      .on("mouseover", function() {
+      .on("mouseover", function(d) {
+        console.log(d)
+         var htmlstring =  "<h5> Schedule </h5>"
+    for (var property in d) {
+      if (d.hasOwnProperty(property) && property != "train_name") {
+        // do stuff
+        if (d[property] != "") {
+          htmlstring += property + " : " + d[property] + "<br>"
+        }
+      }
+    }
+    console.log(htmlstring);
         d3.select(this)
         .style("stroke", "orange");    
         
@@ -126,9 +158,6 @@ d3.csv("../data/hyperloop.csv", function(data) {
               .duration(200)    
               .style("opacity", "0"); 
       })
-      //.append("svg:title")
-      //.text(string);
-    }
 });
   
 
